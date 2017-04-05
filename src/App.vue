ea222e6652ecb5367afc8611a83c8ab5aed2d7cf
orangeC <18332563755@163.com>
2017-04-05 17:14:41 +0800

    小试

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+    <i-switch v-model="switch1" @on-change="change"></i-switch>
+    </div>
     <router-view></router-view>
+    <router-link to="/">hello</router-link>
+    <router-link to="/bye">bye</router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      switch1: false
+    }
+  },
+  methods: {
+    change (status) {
+      this.$Message.info('开关状态：' + status)
+    }
+  }
 }
 </script>
 
